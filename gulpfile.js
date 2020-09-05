62d87295ce91bd62742d6254f55699d20c3d629f
Ibarsuk <ivanbarsukov7@gmail.com>
2020-09-05 12:10:45 +0300

    .

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,8 @@ gulp.task("copy", function(){
  return gulp.src([
  "source/fonts/**/*.{woff,woff2}",
  "source/img/**",
- "source/*.ico"
+ "source/*.ico",
+ "source/css/**"
  ], {
  base: "source"
  })
