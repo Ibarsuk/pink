225d0b6ab83d7e75ba545b2021c815b40e7a5eb6
Ibarsuk <ivanbarsukov7@gmail.com>
2021-04-28 15:28:35 +0300

    a bit better js

--- source/js/contest-form.js
+++ source/js/contest-form.js
@@ -1,19 +1,18 @@
 var form = document.querySelector('.page-main__contest-form');
-var formSubmit = document.querySelector('.contest-form__submit-button');
+var formSubmit = form.querySelector('.contest-form__submit-button');
 var pageSuccessPopup = document.querySelector('.page-success');
-var pageSuccessClose = document.querySelector('.page-success__close');
+var pageSuccessClose = pageSuccessPopup.querySelector('.page-success__close');
 var pageErrorPopup = document.querySelector('.page-error');
-var pageErrorClose = document.querySelector('.page-error__close');
+var pageErrorClose = pageErrorPopup.querySelector('.page-error__close');
 
 formSubmit.addEventListener("click", function(evt) {
-  if (form.checkValidity() === true) {
-    evt.preventDefault();
-    pageSuccessPopup.classList.remove('hidden');
-    formSubmit.disabled = true;
-  } else {
+  if (!form.checkValidity()) {
     pageErrorPopup.classList.remove('hidden');
+    return;
   }
-
+  evt.preventDefault();
+  pageSuccessPopup.classList.remove('hidden');
+  formSubmit.disabled = true;
 });
 
 pageSuccessClose.addEventListener("click", function(evt) {
@@ -25,13 +24,13 @@ pageErrorClose.addEventListener("click", function(evt) {
 });
 
 window.addEventListener("keydown", function(evt) {
-  if (evt.keyCode === 27 && pageSuccessPopup.classList.contains('hidden') === false) {
+  if (evt.keyCode === 27 && !pageSuccessPopup.classList.contains('hidden')) {
       pageSuccessPopup.classList.add('hidden');
     }
 });
 
 window.addEventListener("keydown", function(evt) {
-  if (evt.keyCode === 27 && pageErrorPopup.classList.contains('hidden') === false) {
+  if (evt.keyCode === 27 && !pageErrorPopup.classList.contains('hidden')) {
       pageErrorPopup.classList.add('hidden');
     }
 });
--- source/js/nav-close.js
+++ source/js/nav-close.js
@@ -3,15 +3,10 @@ var mainNav = document.querySelector('.main-nav');
 var logoButton = document.querySelector('.page-header__logo-button');
 
 window.onload = function () {
-  mainNav.classList.remove('main-nav--opened')
+  mainNav.classList.remove('main-nav--opened');
 }
 
 navButton.addEventListener("click", function(evt) {
-  if (mainNav.classList.contains('main-nav--opened')) {
-    mainNav.classList.remove('main-nav--opened');
-    logoButton.classList.remove('page-header__logo-button--menu-open')
-  } else {
-    mainNav.classList.add('main-nav--opened');
-    logoButton.classList.add('page-header__logo-button--menu-open')
-  }
+  mainNav.classList.toggle('main-nav--opened');
+  logoButton.classList.toggle('page-header__logo-button--menu-open');
 });
